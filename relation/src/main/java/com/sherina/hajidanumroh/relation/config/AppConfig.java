729ee1187f6a8c49cd66e18759db8fae7480b31d
sherina brillian <sherina.brillian@quatronema.co.id>
2024-04-16 13:37:30 +0700

    Update

---
+++ relation/src/main/java/com/sherina/hajidanumroh/relation/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.sherina.hajidanumroh.relation.config;
+
+public class AppConfig {
+    
+}
---
+++ relation/src/main/java/com/sherina/hajidanumroh/relation/constants/Constant.java
@@ -0,0 +1,5 @@
+package com.sherina.hajidanumroh.relation.constants;
+
+public class Constant {
+    
+}
